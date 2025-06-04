33a7a545b84181e185bca4f9e870b460add855ff
Anthony Huang <anthonyhuang1909@gmail.com>
2025-06-04 23:11:48 +0800

    new project

--- .history/main_20250604101425.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-# Memuat file .npy
-data = np.load('file_data.npy')
-
-# Cek bentuk (shape) data untuk melihat struktur array
-print(data.shape)
--- .history/main_20250604101459.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-# Memuat file .npy
-data = np.load('/Users/anthonyhuang/Desktop/project/seis2_1_0.npy')
-
-# Cek bentuk (shape) data untuk melihat struktur array
-print(data)
